ee31c3baf2f1056377bc8308ec8bf759ecd48df1
Alice Campkin <alice.campkin@gmail.com>
2022-12-10 18:16:48 +0000

    Customises user model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.core.validators import validate_email
+class CustomUserManager(UserManager):
 
-from django.contrib.auth.models import AbstractUser
+    def _get_email(self, email: str):
+        validate_email(email)
+        return self.normalize_email(email)
+
+    def _create_user(
+        self, 
+        email: str, 
+        password: str, 
+        is_staff: bool = False, 
+        is_superuser: bool = False
+    ):
+        
+        email = self._get_email(email)
+        
+        user = User(email=email, username=email, is_staff=is_staff, is_superuser=is_superuser)
+        user.set_password(password)
+        user.save()
+        
+        return user
+
+    def create_superuser(self, email: str, password: str):
+        return self._create_user(email, password, is_staff=True, is_superuser=True)
+    
+    def create_user(self, email: str, password: str):
+        return self._create_user(email, password)
 
 class User(AbstractUser):
-    pass
+
+    email = models.EmailField(unique=True, blank=False, null=False)
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
